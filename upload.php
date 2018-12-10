ee2e02541a83d3dcebe7d3fe4c849a2d282c89fc
stenmarkus <stenmarkus@users.noreply.github.com>
2018-12-10 02:25:58 +0200

    viimane vb

--- upload.php
+++ upload.php
@@ -8,6 +8,18 @@
 <?php ob_start(); include 'css/main.css'; ?>
 </style>
 <?php session_start(); include("header.php"); ?>
+<div class = "search">
+	<form method = "post" enctype = "multipart/form-data">
+		<select name="values">
+			<option value="haagis">Haagise nimi</option>
+			<option value="omanik">Omaniku nimi</option>
+		</select>
+		<input type = "text" name = "otsing">
+		<input type = "submit" value = "Otsi" name ="search">
+		
+		
+	</form>
+</div>
 <div class = "header">
 	<div id="myModal" class="modal">
 		<div class="modal-content">
@@ -42,6 +54,127 @@
 
 	</div>
 </div>
+
+<div class = "header">
+	<div id="proov" class="modal">
+		<div class="modal-content">
+			<span class="close">&times;</span>
+			<p><?php 
+			require "../../config.php";
+			$database = "if17_lahtsten";
+			$trailer = $_SESSION['trailer'];
+			$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+			$query = "SELECT onehour, twelvehours, oneday, oneweek, onemonth FROM trailerprices INNER JOIN trailerinfo ON trailerprices.trailerId = trailerinfo.trailerId WHERE trailerinfo.trailername = 'jfe'";
+			$result = $mysqli->query($query);
+			$row = $result->fetch_assoc();
+			echo "Ühe tunni hind:" . $row['onehour'] . "€<br> 12 tunni hind: " . $row['twelvehours'] . "€<br> Ühe nädala hind:" . $row['oneweek'] . "€<br> Ühe kuu hind: " . $row['onemonth'] . "€<br>"; 
+			
+		
+		
+			?>
+		
+			</p>
+		  </div>
+
+	</div>
+</div>
+
+
+</body>
+</html>
+
+<?php
+require "../../../config.php";
+if(!isset($_SESSION['username'])){
+	header('Location: frontpage.php');
+}
+
+$database = "if17_lahtsten";
+$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+$sessEmail = $_SESSION['email'];
+$query = " SELECT FirstName, LastName, trailerinfo.trailername, trailerinfo.trailerdesc, trailerinfo.trailerpic, trailerusers.Email FROM trailerusers INNER JOIN trailerinfo ON trailerusers.Email = trailerinfo.Email WHERE trailerinfo.Email <> '$sessEmail'";
+$result = $mysqli->query($query);
+$result_rows = $result->num_rows;
+$loendur = 0;
+for($x = 0; $x<$result_rows; $x++){
+	$loendur = $loendur +1;
+	if(isset($_POST['search'])){
+		break;
+	}
+	if ($result_rows > 0) {
+		$row = $result->fetch_assoc();
+		$trname = $row['trailername'];
+		echo "<div class = 'trailersContainer'><div class = 'addTrailer'><br><br><img src =". $row['trailerpic']. " width = '300' height = '200' id = " . str_replace(' ', '_', $row['trailername']) ."><br>";
+		echo "Üleslaadija: ". $row["FirstName"] . " " . $row["LastName"] . "<br>  Haagise nimi: ". $row["trailername"]. "<br> Haagise kirjeldus: ". $row["trailerdesc"]. "";
+		echo "<p class = 'pricelist' id = 'test" . $loendur . "' >Täpsem hinnakiri</p>";
+		echo "<form method='post'><div class = 'upload-btn-wrapper'><input type = 'submit' value = 'Broneeri' name = " .str_replace(' ', '_', $row['trailername']) . "> </form></div></div></div>";
+		$corstr = str_replace(' ', '_', $row['trailername']);
+		if(isset($_POST["$corstr"])){
+			$fname = $row['FirstName'];
+			$lname = $row['LastName'];
+			$trname = $row['trailername'];
+			$userEmail = $row['Email'];
+			$_SESSION['trailername'] = $trname;
+			$_SESSION['fname'] = $fname;
+			$_SESSION['lname'] = $lname;
+			$_SESSION['meil'] = $row['Email'];
+			header("Location: broneering.php");
+		}
+			
+			
+			
+	}
+}
+
+if(isset($_POST['search'])){
+	$boxname = $_POST['otsing'];
+	if($_POST['values'] == 'haagis'){
+		$query = " SELECT FirstName, LastName, trailerinfo.trailername, trailerinfo.trailerdesc, trailerinfo.trailerpic, trailerusers.Email FROM trailerusers INNER JOIN trailerinfo ON trailerusers.Email = trailerinfo.Email WHERE trailername LIKE '%$boxname%'";
+	}
+	if($_POST['values'] == 'omanik'){
+		$query = "SELECT FirstName, LastName, CONCAT(FirstName, ' ', LastName) AS 'FullName' ,trailerinfo.trailername, trailerinfo.trailerdesc, trailerinfo.trailerpic, trailerusers.Email FROM trailerusers INNER JOIN trailerinfo ON trailerusers.Email = trailerinfo.Email WHERE CONCAT(FirstName, ' ', LastName) LIKE '%$boxname%'"; 
+	}
+	$result = $mysqli->query($query);
+	$result_rows = $result->num_rows;
+	$loendur = 0;
+	for($x = 0; $x<$result_rows; $x++){
+		$loendur = $loendur +1;
+		if ($result_rows > 0) {
+			$row = $result->fetch_assoc();
+			$trname = $row['trailername'];
+			echo "<div class = 'trailersContainer'><div class = 'addTrailer'><br><br><img src =". $row['trailerpic']. " width = '300' height = '200' id = " . str_replace(' ', '_', $row['trailername']) ."><br>";
+			echo "Üleslaadija: ". $row["FirstName"] . " " . $row["LastName"] . "<br>  Haagise nimi: ". $row["trailername"]. "<br> Haagise kirjeldus: ". $row["trailerdesc"]. "<br>";
+			echo "<p class = 'pricelist' id = 'test" . $loendur . "' >Täpsem hinnakiri</p>";
+			echo "<form method='post'><div class = 'upload-btn-wrapper'><input type = 'submit' value = 'Broneeri' name = " .str_replace(' ', '_', $row['trailername']) . "> </form></div></div></div>";
+			$corstr = str_replace(' ', '_', $row['trailername']);
+			if(isset($_POST["$corstr"])){
+				$fname = $row['FirstName'];
+				$lname = $row['LastName'];
+				$trname = $row['trailername'];
+				$userEmail = $row['Email'];
+				$_SESSION['trailername'] = $trname;
+				$_SESSION['fname'] = $fname;
+				$_SESSION['lname'] = $lname;
+				$_SESSION['meil'] = $row['Email'];
+				header("Location: broneering.php");
+			}
+			
+			
+			
+		}
+	}
+}
+if(isset($_POST['submit'])){
+	session_unset();
+	header("Location:frontpage.php");
+}
+include 'footer.php';
+?>
+<div id="dom-target" style="display: none;">
+    <?php 
+        echo htmlspecialchars($loendur); 
+    ?>
+</div>
 <script>
 // Get the modal
 var modal = document.getElementById('myModal');
@@ -68,51 +201,33 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-</script>
 
-</body>
-</html>
+var div = document.getElementById('dom-target');
+var myData = div.textContent;
+for (i = 1; i<myData; i++){
+	
+	var btn2 = document.getElementById('test' + i);
+	
+	var modal2 = document.getElementById('proov');
 
-<?php
-require "../../config.php";
-if(!isset($_SESSION['username'])){
-	header('Location: frontpage.php');
-}
 
-$database = "if17_lahtsten";
-$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
-$sessEmail = $_SESSION['email'];
-$query = " SELECT FirstName, LastName, trailerinfo.trailername, trailerinfo.trailerdesc, trailerinfo.trailerpic, trailerusers.Email FROM trailerusers INNER JOIN trailerinfo ON trailerusers.Email = trailerinfo.Email WHERE trailerinfo.Email <> '$sessEmail'";
-$result = $mysqli->query($query);
-$result_rows = $result->num_rows;
-for($x = 0; $x<$result_rows; $x++){
-	if ($result_rows > 0) {
-		$row = $result->fetch_assoc();
-		$trname = $row['trailername'];
-		echo "<div class = 'trailersContainer'><div class = 'addTrailer'><br><br><img src =". $row['trailerpic']. " width = '300' height = '200' id = " . str_replace(' ', '_', $row['trailername']) ."><br>";
-		echo "Üleslaadija: ". $row["FirstName"] . " " . $row["LastName"] . "<br>  Haagise nimi: ". $row["trailername"]. "<br> Haagise kirjeldus: ". $row["trailerdesc"]. "";
-		echo "<form method='post'><div class = 'upload-btn-wrapper'><input type = 'submit' value = 'Broneeri' name = " .str_replace(' ', '_', $row['trailername']) . "> </form></div></div></div>";
-		$corstr = str_replace(' ', '_', $row['trailername']);
-		if(isset($_POST["$corstr"])){
-			$fname = $row['FirstName'];
-			$lname = $row['LastName'];
-			$trname = $row['trailername'];
-			$userEmail = $row['Email'];
-			$_SESSION['trailername'] = $trname;
-			$_SESSION['fname'] = $fname;
-			$_SESSION['lname'] = $lname;
-			$_SESSION['meil'] = $row['Email'];
-			header("Location: broneering.php");
-		}
-			
-			
-			
+	var span2 = document.getElementsByClassName("close")[1];
+	
+	console.log(span2);
+
+	btn2.onclick = function() {
+		modal2.style.display = "block";
 	}
-}
-if(isset($_POST['submit'])){
-	session_unset();
-	header("Location:frontpage.php");
-}
-include 'footer.php';
-?>
+	span2.onclick = function(){
+		modal2.style.display = "none";
+	}
 
+
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal2.style.display = "none";
+		}
+	}
+		
+}
+</script>
